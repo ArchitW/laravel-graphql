1626427ce37736c02e8cbac8ae20a33e00006943
architw <archit2x3@hotmail.com>
2019-06-29 19:31:44 +0530

    relationships for book and category

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected  $guarded=[];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
