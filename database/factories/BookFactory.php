4f6ca0395e76e0a48276a9c740bbc964fca7b33a
architw <archit2x3@hotmail.com>
2019-06-29 19:26:34 +0530

    model for Books and Category

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Book;
+use Faker\Generator as Faker;
+
+$factory->define(Book::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
